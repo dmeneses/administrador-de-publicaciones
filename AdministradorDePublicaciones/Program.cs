3e8311eb4c14eb633f575ba3535bb88f0cf54efd
Daniela Meneses <dvargas@oildex.com>
2021-03-05 12:45:01 -0400

    Initial integration tests for administrador de publicaciones.

---
+++ AdministradorDePublicaciones/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SourceCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class AdministradorDePublicaciones
+    {
+        public List<Ciudadano> Ciudadanos { get; set; }
+
+        public AdministradorDePublicaciones()
+        {
+            Ciudadanos = new List<Ciudadano>();
+        }
+
+
+        public void agregarCiudadano(Ciudadano ciudadano)
+        {
+            Ciudadanos.Add(ciudadano);
+        }
+    }
+
+    public class Ciudadano
+    {
+        public List<Publicacion> ListaDePublicaciones { get; set; }
+
+        public Ciudadano()
+        {
+            ListaDePublicaciones = new List<Publicacion>();
+        }
+
+        public void agregarPublicacion(Publicacion publicacion)
+        {
+            ListaDePublicaciones.Add(publicacion);
+        }
+    }
+
+    public class Publicacion
+    {
+        public string Contenido { get; set; }
+
+        public Publicacion(string contenido)
+        {
+            Contenido = contenido;
+        }
+    }
+}
+
---
+++ Tests/AdministradorDePublicacionesTest.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using NUnit.Framework;
+using SourceCode;
+
+namespace Tests
+{
+    public class AdministradorDePublicacionesTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DeberiaCrearUnaPublicacion()
+        {
+            var contenido = "publicacion 1";
+            var administrador = new AdministradorDePublicaciones();
+            var ciudadano = new Ciudadano();
+            var publicacion = new Publicacion(contenido);
+            administrador.agregarCiudadano(ciudadano);
+            ciudadano.agregarPublicacion(publicacion);
+
+            Assert.AreEqual(ciudadano.ListaDePublicaciones.Count, 1);
+            Assert.AreEqual(ciudadano.ListaDePublicaciones.First().Contenido, contenido);
+        }
+    }
+}
